286154ad65bd3bd2b1e1d1be53ffdb00ac9234ed
Md Nazrul Islam <nazrul@nascenia.com>
2012-11-20 00:01:18 +0600

    fix total amount nill error

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,5 +1,9 @@
 module BillsHelper
   def total(bill)
-    total = bill.house_rent + bill.gas_bill + bill.meal + bill.service + bill.extra
+    if(!bill.nil?)
+      total = bill.house_rent + bill.gas_bill + bill.meal + bill.service + bill.extra
+    else
+      0
+    end
   end
 end
